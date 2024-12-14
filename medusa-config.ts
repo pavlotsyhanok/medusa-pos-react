31beec564c6eb1b5d7d6cecda9b19f0f540c0e98
Pavlo Tsyhanok <pavlotsyhanok@gmail.com>
2024-12-14 12:32:25 -0800

    added config for could deployment

--- medusa-config.ts
+++ medusa-config.ts
@@ -5,6 +5,8 @@ loadEnv(process.env.NODE_ENV || 'development', process.cwd())
 module.exports = defineConfig({
   projectConfig: {
     databaseUrl: process.env.DATABASE_URL,
+    redisUrl: process.env.REDIS_URL,
+    workerMode: process.env.MEDUSA_WORKER_MODE as "shared" | "worker" | "server",
     http: {
       storeCors: process.env.STORE_CORS!,
       adminCors: process.env.ADMIN_CORS!,
@@ -24,6 +26,14 @@ module.exports = defineConfig({
       resolve: "@medusajs/medusa/event-bus-redis", 
       options: {
         redisUrl: process.env.REDIS_URL,
+      },
+    },
+    {
+      resolve: "@medusajs/medusa/workflow-engine-redis",
+      options: {
+        redis: {
+          url: process.env.REDIS_URL,
+        },
       },
     },
   ]

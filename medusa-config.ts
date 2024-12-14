662a306e1dfbd174b465030f3ad8294976456962
Pavlo Tsyhanok <pavlotsyhanok@gmail.com>
2024-12-14 12:07:33 -0800

    Initial commit

---
+++ medusa-config.ts
@@ -0,0 +1,30 @@
+import { loadEnv, defineConfig, Modules } from '@medusajs/framework/utils'
+
+loadEnv(process.env.NODE_ENV || 'development', process.cwd())
+
+module.exports = defineConfig({
+  projectConfig: {
+    databaseUrl: process.env.DATABASE_URL,
+    http: {
+      storeCors: process.env.STORE_CORS!,
+      adminCors: process.env.ADMIN_CORS!,
+      authCors: process.env.AUTH_CORS!,
+      jwtSecret: process.env.JWT_SECRET || "supersecret",
+      cookieSecret: process.env.COOKIE_SECRET || "supersecret",
+    }
+  },
+  modules: [
+    {
+      resolve: "@medusajs/medusa/cache-redis",
+      options: {
+        redisUrl: process.env.REDIS_URL,
+      },
+    },
+    {
+      resolve: "@medusajs/medusa/event-bus-redis", 
+      options: {
+        redisUrl: process.env.REDIS_URL,
+      },
+    },
+  ]
+})
---
+++ src/api/admin/custom/route.ts
@@ -0,0 +1,8 @@
+import { MedusaRequest, MedusaResponse } from "@medusajs/framework/http";
+
+export async function GET(
+  req: MedusaRequest,
+  res: MedusaResponse
+): Promise<void> {
+  res.sendStatus(200);
+}

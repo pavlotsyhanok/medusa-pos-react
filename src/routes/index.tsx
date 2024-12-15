6837c372be3d69b2c39b043a00123e0181ca77ee
Pavlo Tsyhanok <pavlotsyhanok@gmail.com>
2024-12-14 19:16:02 -0800

    added tailwind css to the project

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -8,7 +8,7 @@ export const Route = createFileRoute('/')({
 function HomeComponent() {
   return (
     <div className="p-2">
-      <h3>Welcome Home!</h3>
+      <h3 className="text-4xl font-bold">Welcome Home!</h3>
     </div>
   )
 }

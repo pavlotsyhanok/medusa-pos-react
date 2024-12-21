5bed6cf256200a751b2967f41a6b3ce88f8ee96e
Pavlo Tsyhanok <pavlotsyhanok@gmail.com>
2024-12-21 11:04:02 -0800

    implemented redirect for not found routes

--- src/router.tsx
+++ src/router.tsx
@@ -1,11 +1,24 @@
-import { createRouter } from '@tanstack/react-router'
+import { createRouter, createRoute, redirect } from '@tanstack/react-router'
 import { routeTree } from './routeTree.gen'
+
+const notFoundRoute = createRoute({
+  getParentRoute: () => routeTree,
+  path: '*',
+  beforeLoad: ({ context }) => {
+    if (context.auth.isAuthenticated) {
+      throw redirect({
+        to: '/store'
+      })
+    }
+  }
+})
 
 export const router = createRouter({
   routeTree,
   context: {
     auth: undefined!,
   },
+  notFoundRoute,
 })
 
 declare module '@tanstack/react-router' {
